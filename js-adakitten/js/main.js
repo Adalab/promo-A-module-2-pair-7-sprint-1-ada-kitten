575d4f3cde9ebd198ad373daa8ee9d35f3d525f8
AntiaNavajasGarrido <antianavajasgarrido@gmail.com>
2023-12-02 10:32:30 +0100

    adding more functions to js

--- js-adakitten/js/main.js
+++ js-adakitten/js/main.js
@@ -1,21 +1,22 @@
 'use strict';
 
-const kittenList = document.querySelector ('.js_list');
+const kittenList = document.querySelector('.js_list');
 
-const kittenOne= `<li class="card">
+const kittenImage1 = 'https://dev.adalab.es/gato-siames.webp';
+const kittenName1 = 'Anastacio';
+const kittenBreed1 = 'Siamés';
+const kittenDesc1 = ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+
+const kittenOne = `<li class="card">
 <article>
   <img
     class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
+    src="${kittenImage1}"
     alt="gatito"
   />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
+  <h3 class="card_title">${kittenName1}</h3>
+  <h4 class="card_race">${kittenBreed1}</h4>
+  <p class="card_description">${kittenDesc1}</p>
 </article>
 </li>`;
 
@@ -30,7 +31,7 @@ const kittenTwo = `<li class="card">
 </p>
 </li>`;
 
-const kittenThree=  `<li class="card">
+const kittenThree = `<li class="card">
 <img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp" alt="maine-coon-cat" />
 <h3 class="card_title">Cielo</h3>
 <h4 class="card_race">Maine Coon</h4>
@@ -41,23 +42,44 @@ const kittenThree=  `<li class="card">
 </p>
 </li>`;
 
-kittenList.innerHTML= kittenOne + kittenTwo + kittenThree;
+kittenList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
-const formSection = document.querySelector ('.js_formSection');
+const formSection = document.querySelector('.js_formSection');
 
-const addButton = document.querySelector ('.js_addButton');
+const addButton = document.querySelector('.js_addButton');
 
-addButton.addEventListener ('click', (event) => {formSection.classList.toggle ('collapsed')});
+// addButton.addEventListener('click', (event) => { formSection.classList.toggle('collapsed') });
 
-const btnAdd = document.querySelector ('.js-btn-add');
+// El toggle tiene dentro como función las siguientes líneas de código
+
+addButton.addEventListener("click", handleClickNewCatForm);
+
+function showNewCatForm() {
+  formSection.classList.remove("collapsed");
+}
+function hideNewCatForm() {
+  formSection.classList.add("collapsed");
+}
+function handleClickNewCatForm(event) {
+  event.preventDefault();
+  if (formSection.classList.contains("collapsed")) {
+    showNewCatForm();
+  } else {
+    hideNewCatForm();
+  }
+}
+
+//hasta aquí(toggle)
+
+const btnAdd = document.querySelector('.js-btn-add');
 
 const inputPhoto = document.querySelector('.js-input-photo');
 const inputName = document.querySelector('.js-input-name');
 const inputBreed = document.querySelector('.js-input-breed');
 const inputDesc = document.querySelector('.js-input-desc');
 
-btnAdd.addEventListener ('click', (event) => {
-  
+btnAdd.addEventListener('click', (event) => {
+
   event.preventDefault();
 
   //para quitar el submit del botón "Añadir"
@@ -71,14 +93,12 @@ btnAdd.addEventListener ('click', (event) => {
   <article>
     <img
       class="card_img"
-      src="` + valuePhoto + `"
+      src="${valuePhoto}"
       alt="gatito"
     />
-    <h3 class="card_title">` + valueName + `</h3>
-    <h4 class="card_race">` + valueBreed + `</h4>
-    <p class="card_description">
-        ` + valueDesc + `
-     </p>
+    <h3 class="card_title">${valueName}</h3>
+    <h4 class="card_race">${valueBreed}</h4>
+    <p class="card_description">${valueDesc}</p>
   </article>
   </li>`;
 
